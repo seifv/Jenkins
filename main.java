bbdb067b4d898b4d12c8c4921cba711b47d06f8d
seifv <100715313+seifv@users.noreply.github.com>
2023-01-12 12:01:05 +0100

    Update main.java

--- main.java
+++ main.java
@@ -1,4 +1,5 @@
-public class Simple{
-public static void main(String args[]){
-  System.out.println("hello javatpoint");
-}}
+public class Main{
+  public static void main(String args[]){
+    System.out.println("Test");
+  }
+}
